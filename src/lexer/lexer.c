19d55c71459c3f2ef7b51c67a27daa74886623f8
Cadavanaugh <jovanecastro12@gmail.com>
2026-02-13 14:45:27 -0300

    REFACTOR: Upgrade `were_quotes_closed` function
    It now detects nested quotes (ex: "simple 'quote inside")

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -21,30 +21,46 @@ static t_token *create_token_list(char **str_tokens)
   return token_list;
 }
 
-static char were_quotes_closed(char *input)
+static char were_quotes_closed(char *input, size_t i)
 {
-  int i;
-  int double_quote_count;
-  i = 0;
+  size_t double_quote_count;
+  size_t simple_quote_count;
+  char inside_double_quotes;
+  char inside_simple_quotes;
+  inside_double_quotes = 0;
+  inside_simple_quotes = 0;
+  simple_quote_count = 0;
   double_quote_count = 0;
   while (input[i])
   {
-    if (input[i] == '"')
+    if (input[i] == '"' && inside_simple_quotes == 0)
+    {
       double_quote_count++;
+      inside_double_quotes = boolean_invert(inside_double_quotes);
+    }
+    if (input[i] == '\'' && inside_double_quotes == 0)
+    {
+      simple_quote_count++;
+      inside_simple_quotes = boolean_invert(inside_simple_quotes);
+    }
     i++;
   }
-  return (double_quote_count % 2 == 0);
+  return (double_quote_count % 2 == 0 && simple_quote_count % 2 == 0);
 }
 
 void lexit(char *input)
 {
-  if (!were_quotes_closed(input))
+  if (!were_quotes_closed(input, 0))
     printf("Aspas não fechadas\n");
   else {
     char **words;
     words = word_splitter(input);
     t_token *token_list;
     token_list = create_token_list(words);
+    for (int x = 0; x < array_length(words); x++)
+    {
+      printf("TYPE %d: %s\n", token_list[x].type, token_list[x].value);
+    }
     (void)token_list;
   }
 }
