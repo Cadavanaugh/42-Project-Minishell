a7c14e1c0857dc9cde1ae85643de90ac8ccc8690
Jovane Castro <jovanecastro12@gmail.com>
2026-02-19 17:13:59 -0300

    FIX(libft): header import path

--- src/minishell.h
+++ src/minishell.h
@@ -3,7 +3,7 @@
 
 # include <stdio.h>
 # include <stdlib.h>
-# include "libs/libft/libft.h"
+# include "../libs/libft/libft.h"
 //# include <signal.h>
 //# include <unistd.h>
 //# include <sys/types.h>
