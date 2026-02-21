1b676c9f1e06bd3bd624ad6ad3a5af822ba524a5
Cadavanaugh <jovanecastro12@gmail.com>
2026-02-21 14:45:54 -0300

    STYLE(parser): file formatting and cleanup

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,11 +1,5 @@
 #include "../minishell.h"
 
-// 1-contar quantidade de args até pipe ou null
-// 2-alocar e preencher array de args da struct t_cmd
-// 3-parsear (cria a lista de comandos, conta, aloca em quantos * precisar)
-
-// **IMAGINAR COM EXEMPLO 'ls -l'
-
 static int count_args(t_token *token_list, int i)
 {
     int arg_count;
@@ -94,54 +88,29 @@ void debug_shell(t_cmd *head)
 
 t_cmd *parser(t_token *token_list)
 {
-    t_cmd   *head = NULL;  // O primeiro comando (o que você retorna)
-    t_cmd   *last = NULL;  // O ajudante para conectar os nós
+    t_cmd   *head = NULL;
+    t_cmd   *last = NULL;
     int     i = 0;
     int     total_args;
-
     if (!token_list)
         return (NULL);
-
     while (token_list[i].value != NULL)
     {
-        // 1. Cria o novo nó
         t_cmd *new = ft_calloc(sizeof(t_cmd), 1);
         if (!new)
-            return (NULL); // Idealmente, aqui você daria free na lista já criada
-
-        // 2. LOGICA DE CONEXÃO (O "Engate")
-        if (head == NULL)   // Se é o primeiro comando da linha
+            return (NULL);
+        if (head == NULL)
             head = new;
-        else                // Se já temos comandos antes, conecta o anterior a este
+        else
             last->next = new;
-        last = new;         // O novo agora passa a ser o último
-
-        // 3. Preenchimento dos dados (como você já fez)
+        last = new;
         total_args = count_args(token_list, i);
         new->args = ft_calloc(sizeof(char *), total_args + 1);
         if (!new->args)
             return (NULL);
-        
         parse_token_to_cmd(token_list, new, &i);
-
-        // 4. O Pulo do Pipe:
-        // Se paramos em um PIPE, precisamos saltar ele para o 
-        // próximo loop criar o próximo comando.
         if (token_list[i].value != NULL && token_list[i].type == PIPE)
             i++;
-
-        printf("command: %s\n", (new->args)[0]);
-        int j = 1;
-        while ((new->args)[j] != NULL)
-        {
-            printf("args: %s\n", (new->args)[j]);
-            j++;
-        }
-
     }
-    return (head); // Retorna a cabeça da lista para o executor
+    return (head);
 }
-
-//fiquei em duvida do porque tá sempre usando o i como indice, 
-//e iterando pelo proprio ponteiro em si
-//precisa tratar casos com | e redir
