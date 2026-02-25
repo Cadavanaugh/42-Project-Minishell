8c932d6099deb638dca3912cdff372e28246aef7
Victor <vitorvallete@gmail.com>
2026-02-25 15:01:29 -0300

    FIX: added guard to not accept null dest

--- src/executor/builtins/cd.c
+++ src/executor/builtins/cd.c
@@ -8,10 +8,16 @@ void builtin_cd(char **args, t_ms *shell)
     oldpwd = getcwd(cwd, sizeof(cwd));
     if (!args[1])
         dest = get_env_val("HOME", shell);
-    else if (ft_strncmp(args[1], "-", 1) == 0)
+    else if (ft_strcmp(args[1], "-") == 0 )
         dest = get_env_val("OLDPWD", shell);
     else
         dest = args[1];
+	if (!dest)
+	{
+		ft_putstr_fd("minishell: cd: HOME not set\n", 2);
+		shell->last_status = 1;
+		return ;
+	}
     if (chdir(dest) != 0)
     {
         perror("minishell: cd");
