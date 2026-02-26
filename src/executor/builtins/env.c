0f212dc2778945577b3a49182db2b3145d726b7d
Cadavanaugh <jovanecastro12@gmail.com>
2026-02-26 18:20:35 -0300

    FIX: unset looks for whole word
    Checks the following character to be `=` for proper comparsion.

--- src/executor/builtins/env.c
+++ src/executor/builtins/env.c
@@ -13,21 +13,39 @@ void builtin_env(char **envs)
   }
 }
 
+static char does_env_exist(char **envs, char *name)
+{
+  int i = 0;
+  int len = ft_strlen(name);
+  while(envs[i])
+  {
+    if (ft_strncmp(envs[i], name, len) == 0 && envs[i][len] == '=')
+      return 1;
+    i++;
+  }
+  return 0;
+}
+
 void builtin_unset(char ***envs, char *name)
 {
   int i = 0;
   int j = 0;
+  int max = 0;
   char **new_envs;
   if (!name || !*envs)
     return;
-  while((*envs)[i])
-    i++;
-  new_envs = ft_calloc(i, sizeof(char *));
+  while((*envs)[max])
+    max++;
+  new_envs = ft_calloc(max, sizeof(char *));
+  if (!new_envs || !does_env_exist(*envs, name))
+    return;
   i = 0;
   while((*envs)[i])
   {
-    if (ft_strncmp((*envs)[i], name, ft_strlen(name)) == 0)
+    if (ft_strncmp((*envs)[i], name, ft_strlen(name)) == 0 && (*envs)[i][ft_strlen(name)] == '=')
       i++;
+    if (i == max)
+      break;
     new_envs[j] = (*envs)[i];
     i++;
     j++;
