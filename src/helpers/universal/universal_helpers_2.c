2c340d2b4f7891853558c0cbe6226abe29b68d07
Victor De Souza Valente <victde-s@c3r1p5.42sp.org.br>
2026-03-27 14:56:41 -0300

    chore: norminette corrections

--- src/helpers/universal/universal_helpers_2.c
+++ src/helpers/universal/universal_helpers_2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   universal_helpers_2.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jode-cas <jode-cas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: victde-s <victde-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/02 15:15:37 by jode-cas          #+#    #+#             */
-/*   Updated: 2026/03/24 19:53:08 by jode-cas         ###   ########.fr       */
+/*   Updated: 2026/03/27 14:54:15 by victde-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,11 @@ int	array_length(char **array)
 	return (counter);
 }
 
-void display_error(char *str, const char *error_msg)
+void	display_error(char *str, const char *error_msg)
 {
-	char *result;
+	char	*result;
 
 	result = ft_strjoin(str, error_msg);
-	ft_putstr_fd(result ,2);
+	ft_putstr_fd(result, 2);
 	free(result);
 }
